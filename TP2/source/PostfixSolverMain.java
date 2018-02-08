321426e175e9ec2151fd81f6bf035d1f415eb2af
Valentin Bouis <valentin.bouis@gmail.com>
2018-02-08 02:54:09 -0500

    exo 2 fonctionnel

--- TP2/source/PostfixSolverMain.java
+++ TP2/source/PostfixSolverMain.java
@@ -8,14 +8,37 @@ public class PostfixSolverMain
 		Stack<Double> stack = new Stack<Double>();
 		
 		String s = "25 5 2 * + 15 3 / 5 - +";
-		
+		double a, b;
 		//L'expression est separee en tokens selon les espaces
 		for(String token : s.split("\\s")) 
 		{
 			//A completer
+			switch(token) {
+			case "+" :
+				a = stack.pop();
+				b = stack.pop();
+				stack.push(a + b);
+				break;
+			case "*" :
+				a = stack.pop();
+				b = stack.pop();
+				stack.push(a * b);
+				break;
+			case "-" :
+				a = stack.pop();
+				b = stack.pop();
+				stack.push(b - a);
+				break;
+			case "/" :
+				a = stack.pop();
+				b = stack.pop();
+				stack.push(b / a);
+				break;
+			default:
+				stack.push(Double.parseDouble(token));
+			}
 			
 		}
-     
 		System.out.println("25 + 5*2 + 15/3 - 5 = "+stack.peek());
 		if(stack.peek() == 35)
 			System.out.println("It's all good");
